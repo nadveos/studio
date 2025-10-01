47e31223fd1c001bfc2d0616c6312e448d52d1d6
Gustavo Javier Flores <guta.flores@gmail.com>
2025-10-01 13:54:16 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useActionState, useFormStatus } from "react";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { useEffect } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { createAppointment } from "@/lib/actions";
